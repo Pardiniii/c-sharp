a7944a64159ada53b88bfe01d717ef1fd7368174
Gustavo Guimarães Pardini <104874698+Pardiniii@users.noreply.github.com>
2023-10-10 03:02:26 -0300

    Add files via upload

---
+++ Aprovado ou nao/ConsoleApp4/Aluno.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Runtime.CompilerServices;
+
+public class Aluno
+{
+    int matricula;
+    public string nome;
+    string nomeDisciplina;
+    double notaProva1;
+    double notaProva2;
+    double notaTrabalho;
+
+    public Aluno(int matricula, string nome, string nomeDisciplina, double notaProva1, double notaProva2, double notaTrabalho)
+    {
+        this.matricula = matricula;
+        this.nome = nome;
+        this.nomeDisciplina = nomeDisciplina;
+        this.notaProva1 = notaProva1;
+        this.notaProva2 = notaProva2;
+        this.notaTrabalho = notaTrabalho;
+    }
+
+    public double calcularMedia()
+    {
+        double media = ((this.notaProva1 * 3.5) + (this.notaProva2 * 3.5) + (this.notaTrabalho * 3)) / 10;
+        return media;
+    }
+
+    public bool estaAprovado()
+    {
+        double media = this.calcularMedia();
+
+        if (media >= 6) return true;
+        return false;
+    }
+}
---
+++ Aprovado ou nao/ConsoleApp4/Program.cs
@@ -0,0 +1,9 @@
+﻿Aluno aluno1 = new Aluno(0, "Jose", "POO", 5, 3, 6);
+Aluno aluno2 = new Aluno(1, "Maria", "POO", 7, 8, 9);
+Aluno aluno3 = new Aluno(2, "João", "POO", 1, 2, 6);
+Aluno aluno4 = new Aluno(3, "Claudia", "POO", 10, 4, 6);
+
+Console.WriteLine($"O {aluno1.nome} ficou com media {aluno1.calcularMedia()} e está aprovado? {aluno1.estaAprovado()}");
+Console.WriteLine($"O {aluno2.nome} ficou com media {aluno2.calcularMedia()} e está aprovado? {aluno2.estaAprovado()}");
+Console.WriteLine($"O {aluno3.nome} ficou com media {aluno3.calcularMedia()} e está aprovado? {aluno3.estaAprovado()}");
+Console.WriteLine($"O {aluno4.nome} ficou com media {aluno4.calcularMedia()} e está aprovado? {aluno4.estaAprovado()}");
